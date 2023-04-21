6c0228a9a64b119789432c29bb2416661512b7f5
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-04-21 13:19:22 +0300

    end first kind

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,92 @@ const express = require('express')
 
 const router = express.Router()
 
+const contacts = require("../../models/contacts");
+
+const {HttpError} = require("../../helpers")
+
+const Joi = require('joi');
+
+const addSchema = Joi.object({
+  name:Joi.string().required(),
+  email:Joi.string().required(),
+  phone:Joi.string().required()
+})
+
+const updateSchema = Joi.object().keys({
+  name:Joi.string(),
+  email:Joi.string(),
+  phone:Joi.string()
+})
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+ try {
+  const result = await contacts.listContacts();
+  res.json(result)
+  console.log(result)
+ } catch (error) {
+  next(error)
+ }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+ try {
+  const {contactId} = req.params;
+   const result = await contacts.getContactById(contactId);
+   if(!result){
+    throw HttpError(404,"Not found")
+   }
+   res.json(result)
+ } catch (error) {
+  next(error)
+  
+ }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = addSchema.validate(req.body);
+    if(error){
+      throw HttpError(400,error.message)
+    }
+    const {name,email,phone} = req.body;
+    const result = await contacts.addContact(name,email,phone);
+    console.log(result)
+    res.status(201).json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+   const {contactId} = req.params;
+   const result = contacts.removeContact(contactId);
+   if(!result){
+    throw HttpError(404,"Not found")
+   }
+   res.json({message:"Delete success"})
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+ try {
+  const keys = Object.keys(req.body);
+  if (keys.length === 0) {
+   throw HttpError(400, "missing fields");
+}
+    const {error} = updateSchema.validate(req.body)
+    if(error){
+       throw HttpError(400, error.message)
+    }
+  const { contactId } = req.params;
+  const body = req.body
+  const result = await contacts.updateContact(contactId, body);
+  res.json(result)
+ } catch (error) {
+  next(error)
+ }
 })
 
 module.exports = router
