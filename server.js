36678319a89e105eecfb2c7a43399bb212d6e02b
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-04-22 13:44:40 +0300

    connect to db

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const mongoose = require('mongoose');
+const { error } = require('./schema/updateSchema');
+
+const DB_HOST = "mongodb+srv://Bogdan:3xId0DwnHqy03VW3@cluster0.avn2th9.mongodb.net/contacts_read?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+.then(()=>{
+    app.listen(3000);
+    console.log("Server connect success")
 })
+.catch(error=>{
+  console.log(error.message);
+  process.exit(1)
+})
+
+
+
