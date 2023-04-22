900feee8da43de7e5c5462e77d606c641da0ef5f
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-04-22 14:40:31 +0300

    add env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const mongoose = require('mongoose');
 const { error } = require('./schema/updateSchema');
 
-const DB_HOST = "mongodb+srv://Bogdan:3xId0DwnHqy03VW3@cluster0.avn2th9.mongodb.net/contacts_read?retryWrites=true&w=majority";
+const {DB_HOST} = process.env;
 
 mongoose.connect(DB_HOST)
 .then(()=>{
